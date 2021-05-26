7b2e86d03f584ec5d70ecf615dc0407e7c9ad143
AxelOP96 <axeelleguero@gmail.com>
2021-05-26 17:10:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+document.body.style.backgroundColor = '#1cf';
 const colors =["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
